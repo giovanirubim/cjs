aaf79b8981b2632b3b1091532c9adaa244b9eb4c
Giovani <giovanirubim.f@gmail.com>
2024-06-28 12:16:14 -0300

    Remove transpiled code

--- src-js/errors/compilation-error.js
+++
@@ -1,6 +0,0 @@
-export class CompilationError extends Error {
-    constructor(message, index) {
-        super(message);
-        this.index = index;
-    }
-}
--- src-js/model/code-node.js
+++
@@ -1,6 +0,0 @@
-export class CodeNode {
-    constructor(start, end) {
-        this.start = start;
-        this.end = end;
-    }
-}
